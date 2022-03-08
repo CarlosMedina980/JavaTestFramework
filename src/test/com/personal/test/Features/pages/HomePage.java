977176184f2e7140a70bb0f29868187adba98cb5
Carlos Medina <cajameve980@gmail.com>
2022-03-07 21:12:51 -0500

    Made Driver Static to avoid passing as parameter, changed base page to use static driver

--- src/test/com/personal/test/Features/pages/HomePage.java
+++ src/test/com/personal/test/Features/pages/HomePage.java
@@ -1,15 +1,14 @@
 package com.personal.test.Features.pages;
 
 import com.personal.framework.base.BasePage;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class HomePage extends BasePage {
 
-    public HomePage(WebDriver driver) {
-        super(driver);
+    public HomePage() {
+
     }
 
     @FindBy(how = How.ID, using = "loginLink")
