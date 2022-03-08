ca1166f4095ff36a74432624e52f43c3326e9896
CarlosMedina980 <cajameve980@gmail.com>
2022-03-07 21:37:20 -0500

    Added page navigation returning page types (#9)

--- src/test/com/personal/test/Features/pages/EmployeeListPage.java
+++ src/test/com/personal/test/Features/pages/EmployeeListPage.java
@@ -1,15 +1,14 @@
 package com.personal.test.Features.pages;
 
 import com.personal.framework.base.BasePage;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class EmployeeListPage extends BasePage {
 
-    public EmployeeListPage(WebDriver driver) {
-        
+    public EmployeeListPage() {
+
     }
 
     @FindBy(how = How.XPATH, using = "//a[text() = 'Create New']")
