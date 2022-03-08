e491f857d8d9fc9c51777e4d2e2c7c64702d38c4
Carlos Medina <cajameve980@gmail.com>
2022-03-08 16:51:49 -0500

    Added multiple browser added framework initialize and driverContext browser

--- src/com/personal/framework/base/Browser.java
+++ src/com/personal/framework/base/Browser.java
@@ -1,4 +1,22 @@
 package com.personal.framework.base;
 
+import org.openqa.selenium.WebDriver;
+
 public class Browser {
+
+    public WebDriver _driver;
+    public BrowserTypes type;
+
+    public Browser(WebDriver driver) {
+        this._driver = driver;
+    }
+
+    public void GoToUrl(String url) {
+        _driver.get(url);
+    }
+
+    public void Maximize() {
+        _driver.manage().window().maximize();
+    }
 }
+
--- src/com/personal/framework/base/FrameworkInitialize.java
+++ src/com/personal/framework/base/FrameworkInitialize.java
@@ -1,4 +1,32 @@
 package com.personal.framework.base;
 
-public class FrameworkInitialize {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FrameworkInitialize extends Base {
+
+    public void InitializeBrowser(BrowserTypes type) {
+
+        WebDriver Driver = null;
+        switch (type) {
+            case Chrome: {
+                Driver = new ChromeDriver();
+                break;
+            }
+
+            case Firefox: {
+                System.setProperty("webdriver.gecko.driver", "D:\\Libs\\geckodriver.exe");
+                Driver = new FirefoxDriver();
+                break;
+            }
+            case IE: {
+                break;
+            }
+        }
+        //Setting Driver
+        DriverContext.setDriver(Driver);
+        //Setting Browser
+        DriverContext.Browser = new Browser(Driver);
+    }
 }
