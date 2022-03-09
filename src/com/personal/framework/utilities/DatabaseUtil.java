b1302fae4aab22751e2a30973fca6a9b221cdb86
Carlos Medina <cajameve980@gmail.com>
2022-03-09 17:58:05 -0500

    First database changes

--- src/com/personal/framework/utilities/DatabaseUtil.java
+++ src/com/personal/framework/utilities/DatabaseUtil.java
@@ -1,4 +1,32 @@
 package com.personal.framework.utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class DatabaseUtil {
+
+    public void Open(String connectionString) {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            DriverManager.getConnection(connectionString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Close() {
+
+    }
+
+    public void ExecuteQuery(String query, Connection connection) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+        } catch (Exception e) {
+
+        }
+    }
 }
