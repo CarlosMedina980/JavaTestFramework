0559568c327bc0f2267d8eb87aff0b13bc8874c1
Carlos Medina <cajameve980@gmail.com>
2022-03-09 20:31:22 -0500

    Added Config properties for hardcoded staff

--- src/com/personal/framework/config/Settings.java
+++ src/com/personal/framework/config/Settings.java
@@ -1,4 +1,22 @@
 package com.personal.framework.config;
 
+import com.personal.framework.base.BrowserTypes;
+
+import java.sql.Connection;
+
 public class Settings {
+
+    //Application backend connection
+    public static Connection AUTConnection;
+    public static String AUTConnectionString;
+    //Log Path For Framework
+    public static String LogPath;
+    //DriverType for sql server connection
+    public static String DriverType;
+
+    public static String ExcelSheetPath;
+
+    public static String AUT;
+    public static BrowserTypes Browser;
+
 }
