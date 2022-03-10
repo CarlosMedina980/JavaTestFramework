bc88bab04d42182b01dc58bceb7cf1991a2d2dd2
CarlosMedina980 <cajameve980@gmail.com>
2022-03-10 18:54:11 -0500

    Implementing custom selenium controls (#20)
    * FixedDependency
    
    * Added Text And Button Controls

---
+++ src/com/personal/framework/controls/elements/Button.java
@@ -0,0 +1,10 @@
+package com.personal.framework.controls.elements;
+
+import com.personal.framework.controls.internals.Control;
+
+public interface Button extends Control {
+    void PérformClick();
+
+    String GetButtonText();
+
+}
---
+++ src/com/personal/framework/controls/elements/TextBoxBase.java
@@ -0,0 +1,21 @@
+package com.personal.framework.controls.elements;
+
+import com.personal.framework.controls.internals.ControlBase;
+import org.openqa.selenium.WebElement;
+
+public class TextBoxBase extends ControlBase implements TextBox {
+
+    public TextBoxBase(WebElement element) {
+        super(element);
+    }
+
+    @Override
+    public void enterText(String text) {
+        getWrappedElement().sendKeys(text);
+    }
+
+    @Override
+    public String getTextValue() {
+        return getWrappedElement().getText();
+    }
+}
