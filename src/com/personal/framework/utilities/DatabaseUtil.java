8e37807dc434007d988553f31b17bb53b98395a0
CarlosMedina980 <cajameve980@gmail.com>
2022-03-09 19:15:33 -0500

    Database utility implementation (#14)
    * First database changes
    
    * Finished db connection

--- src/com/personal/framework/utilities/DatabaseUtil.java
+++ src/com/personal/framework/utilities/DatabaseUtil.java
@@ -1,4 +1,33 @@
 package com.personal.framework.utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class DatabaseUtil {
+
+    public static Connection Open(String connectionString) {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(connectionString);
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    public static void Close() {
+
+    }
+
+    public static void ExecuteQuery(String query, Connection connection) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+        } catch (Exception e) {
+
+        }
+    }
 }
