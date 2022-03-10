8db01d039e454b7109cd4921af6452452a1153f2
Carlos Medina <cajameve980@gmail.com>
2022-03-10 11:45:27 -0500

    Added first cucumber scenario for login

---
+++ src/test/com/personal/test/seleniumTest/LoginTestSelenium.java
@@ -0,0 +1,20 @@
+package com.personal.test.seleniumTest;
+
+import com.personal.framework.utilities.ExcelUtil;
+import com.personal.test.pages.HomePage;
+import com.personal.test.pages.LoginPage;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginTestSelenium extends SeleniumBaseTest {
+
+
+    @Test
+    public void Login() {
+        CurrentPage = getInstance(HomePage.class);
+        CurrentPage = CurrentPage.AS(HomePage.class).clickLogin();
+        CurrentPage.AS(LoginPage.class).FillLoginData(ExcelUtil.ReadCell("UserName", 1), ExcelUtil.ReadCell("Password", 1));
+        CurrentPage = CurrentPage.AS(LoginPage.class).ClickLogin();
+        Assert.assertEquals("The User Admin did not logged in succesfully", "Hello admin!", CurrentPage.AS(HomePage.class).getUserLoggedIn());
+    }
+}
